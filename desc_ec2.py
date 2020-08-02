6494a357bee637ad50f3c7075bcf2e6608c315fe
annant05 <annantguptaneema@gmail.com>
2020-08-02 23:52:43 +0530

    Completed the basic code

--- desc_ec2.py
+++ desc_ec2.py
@@ -3,11 +3,10 @@ import json
 
 config = json.loads(open("config.json", 'r').read())
 event_json = json.loads(open("cloudwatchevent.json", 'r').read())
-# key = ''
-# secret = ''
-# instance_state = event_json['detail']['state']
 
-# print(instance_id, instance_state)
+# Global Variables
+DOMAIN_NAME = config['DOMAIN_NAME']
+ROUTE53_ZONE_ID = config['ROUTE53_ZONE_ID']
 
 aws_session = boto3.Session(
     region_name=config['AWS_REGION']
@@ -28,7 +27,7 @@ aws_session = boto3.Session(
 def desc_instance(instance_id):
     ec2_client = aws_session.client('ec2')
     ec2_described = ec2_client.describe_instances(
-        InstanceIds=['i-09298412db4b7d4a1'])['Reservations'][0]['Instances'][0]
+        InstanceIds=[instance_id])['Reservations'][0]['Instances'][0]
     print(ec2_described)
 
     return_info = {}
@@ -46,44 +45,73 @@ def desc_instance(instance_id):
     if instance_tags is not None:
         for tag_elem in instance_tags:
             if (tag_elem['Key']).lower() == 'dns':
-                return_info['dns'] = tag_elem['Value']
+                return_info['dns'] = ((tag_elem['Value']).lower()).strip()
 
     return return_info
 
-    def main(instance_id):
-        instance_id = event_json['detail']['instance-id']
 
-    # for elem in instance_tags:
-    #     if (elem['Key']).lower() == 'dns':
-    #         print(elem['Value'])
+def delete_route53_record_set(dns):
+    route53_client = aws_session.client('route53')
 
-    print("end")
+    response = route53_client.change_resource_record_sets(
+        HostedZoneId=ROUTE53_ZONE_ID,
+        ChangeBatch={
+            'Changes': [
+                {
+                    'Action': 'DELETE',
+                    'ResourceRecordSet': {
+                        'Name': f'{dns}.{DOMAIN_NAME}',
+                        'TTL': 300,
+                        'Type': 'A',
+                    },
+                },
+            ],
+        },
+    )
 
-    # running_instances = ec2.instances.filter(Filters=[{
-    #     'Name': 'instance-state-name',
-    #     'Values': ['running']}])
+    print("Deleted record Set", response)
+    return
 
-    # ec2info = defaultdict()
-    # for instance in running_instances:
-    #     for tag in instance.tags:
-    #         if 'Name'in tag['Key']:
-    #             name = tag['Value']
-    #     # Add instance info to a dictionary
-    #     ec2info[instance.id] = {
-    #         'Name': name,
-    #         'Type': instance.instance_type,
-    #         'State': instance.state['Name'],
-    #         'Private IP': instance.private_ip_address,
-    #         'Public IP': instance.public_ip_address,
-    #         'Launch Time': instance.launch_time,
-    #         'Instance ID': instance.id,
-    #         'Public DNS': instance.public_dns_name
-    #         }
 
-    # attributes = ['Name', 'Type', 'State', 'Private IP', 'Public IP', 'Launch Time', 'Instance ID', 'Public DNS']
-    # for instance_id, instance in ec2info.items():
-    #     for key in attributes:
-    #         print("{0}: {1}".format(key, instance[key]))
-    #     print("------")
+def create_route53_record_set(ipv4, dns, instance_id):
+    if ipv4 is None:
+        delete_route53_record_set(dns)
+        return False
 
-    # print('Hello')
+    route53_client = aws_session.client('route53')
+
+    response = route53_client.change_resource_record_sets(
+        HostedZoneId=ROUTE53_ZONE_ID,
+        ChangeBatch={
+            'Comment': f'DNS attached to instance {instance_id}',
+            'Changes': [
+                {
+                    'Action': 'UPSERT',
+                    'ResourceRecordSet': {
+                        'Name': f'{dns}.{DOMAIN_NAME}',
+                        'ResourceRecords': [
+                            {
+                                'Value': ipv4,
+                            },
+                        ],
+                        'TTL': 60,
+                        'Type': 'A',
+                    },
+                },
+            ],
+        },
+    )
+    print("Created record Set", response)
+    return True
+
+
+def main():
+    print("Function START")
+    instance_id = event_json['detail']['instance-id']
+    instance_meta = desc_instance(instance_id)
+    create_route53_record_set(instance_meta['ipv4'], instance_meta['dns'], instance_id)
+    print("Function END")
+
+
+if __name__ == '__main__':
+    main()
